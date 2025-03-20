ca87ae66c85a6c66f738a5987951bff5fcd68b50
chenbo <chbsggdsfd@163.com>
2025-03-20 11:47:59 +0800

    feature：项目初始化

---
+++ SpeedEdit/SpeedEdit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SpeedEdit
+//
+//  Created by 111 on 2025/3/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SpeedEdit/SpeedEdit/SpeedEditApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeedEditApp.swift
+//  SpeedEdit
+//
+//  Created by 111 on 2025/3/20.
+//
+
+import SwiftUI
+
+@main
+struct SpeedEditApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
